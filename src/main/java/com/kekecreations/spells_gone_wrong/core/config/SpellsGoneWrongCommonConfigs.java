22d57ff4542babc43d39849a7d78f0e8b4b26ba0
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-03-07 22:34:56 +0000

    Another Black Hole Feature?

--- src/main/java/com/kekecreations/spells_gone_wrong/core/config/SpellsGoneWrongCommonConfigs.java
+++ src/main/java/com/kekecreations/spells_gone_wrong/core/config/SpellsGoneWrongCommonConfigs.java
@@ -8,10 +8,13 @@ public class SpellsGoneWrongCommonConfigs {
 
     public static final ForgeConfigSpec.ConfigValue<Boolean> BLACK_HOLE_CAN_HURT_OWNER;
 
+    public static final ForgeConfigSpec.ConfigValue<Boolean> BLACK_HOLE_CAN_CAUSE_EXPLOSIONS;
+
+
     static {
         BUILDER.push("Spells Gone Wrong Config");
 
-        BUILDER.comment("Settings for letting a spell hurt the player who casted it  ||  Config changes will only affect newly casted spells");
+        BUILDER.comment("Settings for letting a spell hurt the player who casted it");
         BUILDER.push("Spell Can Hurt Caster Settings");
 
         BLACK_HOLE_CAN_HURT_OWNER = BUILDER
@@ -20,9 +23,20 @@ public class SpellsGoneWrongCommonConfigs {
 
         BUILDER.pop();
 
+        BUILDER.comment("Enable some bonus features for spells!");
+        BUILDER.push("Spell Additions Settings");
+
+        BLACK_HOLE_CAN_CAUSE_EXPLOSIONS = BUILDER
+                .comment("When enabled Black Holes will become unstable just before the spell expires and cause random explosions around it. When enabled Black Holes will also display explosion particles (Don't cause damage) when the spell expires")
+                .define("Black Hole Can Cause Explosions Just Before The Spell Expires", true);
+
+        BUILDER.pop();
 
 
-        //BUILDER.pop();
+
+
+
+        BUILDER.pop();
         SPEC = BUILDER.build();
     }
 }
--- src/main/java/com/kekecreations/spells_gone_wrong/core/mixin/SpellsGoneWrongBlackHoleMixin.java
+++ src/main/java/com/kekecreations/spells_gone_wrong/core/mixin/SpellsGoneWrongBlackHoleMixin.java
@@ -1,7 +1,18 @@
 package com.kekecreations.spells_gone_wrong.core.mixin;
 
 import com.kekecreations.spells_gone_wrong.core.config.SpellsGoneWrongCommonConfigs;
+import io.redspace.ironsspellbooks.capabilities.magic.MagicManager;
 import io.redspace.ironsspellbooks.entity.spells.black_hole.BlackHole;
+import io.redspace.ironsspellbooks.registries.SoundRegistry;
+import io.redspace.ironsspellbooks.util.ParticleHelper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Position;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,9 +27,19 @@ public class SpellsGoneWrongBlackHoleMixin {
         BlackHole blackHole = BlackHole.class.cast(this);
         if (!(blackHole.getOwner() instanceof BlackHole) && SpellsGoneWrongCommonConfigs.BLACK_HOLE_CAN_HURT_OWNER.get()) {
             blackHole.setOwner(blackHole);
-            System.out.println("set owner");
-            System.out.println(blackHole.getOwner());
         }
+        if (blackHole.tickCount > 500 && blackHole.tickCount < 610 && SpellsGoneWrongCommonConfigs.BLACK_HOLE_CAN_CAUSE_EXPLOSIONS.get()) {
+            if ((blackHole.tickCount) % 8 == 0 && !blackHole.level.isClientSide) {
+                BlockPos blockPos = new BlockPos(blackHole.getRandomX(0.3), (blackHole.getRandomY() - 10) + (double)blackHole.getRadius(), blackHole.getRandomZ(0.3));
+                Block block = blackHole.level.getBlockState(blockPos).getBlock();
 
+                if (block == Blocks.AIR || block == Blocks.VOID_AIR || block == Blocks.CAVE_AIR) {
+                    blackHole.level.explode(blackHole, blockPos.getX(), blockPos.getY(), blockPos.getZ(), 4.0F, Explosion.BlockInteraction.BREAK);
+                }
+            }
+        }
+        if (blackHole.tickCount > 610 && SpellsGoneWrongCommonConfigs.BLACK_HOLE_CAN_CAUSE_EXPLOSIONS.get()) {
+            blackHole.getLevel().addParticle(ParticleTypes.EXPLOSION_EMITTER, true, blackHole.getX(), blackHole.getY() + (double)blackHole.getRadius(), blackHole.getZ(), 0, 0, 0);
+        }
     }
 }
