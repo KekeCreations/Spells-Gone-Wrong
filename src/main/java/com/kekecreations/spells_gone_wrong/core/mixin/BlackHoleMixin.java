48fa410f07bf686f1161462da467e8b1407dcc0a
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-03-09 20:23:16 +0000

    Renamed Mixin Classes & Methods

--- src/main/java/com/kekecreations/spells_gone_wrong/core/mixin/BlackHoleMixin.java
+++ src/main/java/com/kekecreations/spells_gone_wrong/core/mixin/BlackHoleMixin.java
@@ -1,15 +1,9 @@
 package com.kekecreations.spells_gone_wrong.core.mixin;
 
 import com.kekecreations.spells_gone_wrong.core.config.SpellsGoneWrongCommonConfigs;
-import io.redspace.ironsspellbooks.capabilities.magic.MagicManager;
 import io.redspace.ironsspellbooks.entity.spells.black_hole.BlackHole;
-import io.redspace.ironsspellbooks.registries.SoundRegistry;
-import io.redspace.ironsspellbooks.util.ParticleHelper;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Position;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -19,11 +13,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlackHole.class)
-public class SpellsGoneWrongBlackHoleMixin {
+public class BlackHoleMixin {
 
 
     @Inject(method = "tick", at = @At(value = "INVOKE", target = "io/redspace/ironsspellbooks/entity/spells/black_hole/BlackHole.getRadius ()F"))
-    public void tick(CallbackInfo ci) {
+    public void spells_gone_wrong_tick(CallbackInfo ci) {
         BlackHole blackHole = BlackHole.class.cast(this);
         if (!(blackHole.getOwner() instanceof BlackHole) && SpellsGoneWrongCommonConfigs.BLACK_HOLE_SPELL_CAN_HURT_OWNER.get()) {
             blackHole.setOwner(blackHole);
--- src/main/java/com/kekecreations/spells_gone_wrong/core/mixin/IceBlockProjectileMixin.java
+++ src/main/java/com/kekecreations/spells_gone_wrong/core/mixin/IceBlockProjectileMixin.java
@@ -18,10 +18,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(IceBlockProjectile.class)
-public class SpellsGoneWrongIceBlockProjectileMixin {
+public class IceBlockProjectileMixin {
 
     @Inject(method = "tick", at = @At(value = "INVOKE", target = "io/redspace/ironsspellbooks/entity/spells/ice_block/IceBlockProjectile.getXRot ()F"))
-    public void tick(CallbackInfo ci) {
+    public void spell_gone_wrong_tick(CallbackInfo ci) {
 
         IceBlockProjectile iceBlockProjectile = IceBlockProjectile.class.cast(this);
         if (!(iceBlockProjectile.getOwner() instanceof IceBlockProjectile) && SpellsGoneWrongCommonConfigs.ICE_BLOCK_SPELL_CAN_HURT_OWNER.get()) {
