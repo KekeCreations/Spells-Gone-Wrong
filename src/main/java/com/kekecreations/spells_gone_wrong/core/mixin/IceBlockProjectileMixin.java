a7ec921023f23467179b2f7503c26584e6b978d8
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-11-18 11:07:56 +0000

    V2.0.1

--- src/main/java/com/kekecreations/spells_gone_wrong/core/mixin/IceBlockProjectileMixin.java
+++ src/main/java/com/kekecreations/spells_gone_wrong/core/mixin/IceBlockProjectileMixin.java
@@ -25,7 +25,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(IceBlockProjectile.class)
 public class IceBlockProjectileMixin {
 
-    @Inject(method = "tick", at = @At(value = "INVOKE", target = "io/redspace/ironsspellbooks/entity/spells/ice_block/IceBlockProjectile.getXRot ()F"))
+
+    @Inject(method = "tick", at = @At(value = "HEAD"))
     public void spells_gone_wrong$tick(CallbackInfo ci) {
         IceBlockProjectile iceBlockProjectile = (IceBlockProjectile) (Object) this;
 
@@ -46,6 +47,7 @@ public class IceBlockProjectileMixin {
             }
         }
     }
+
 
 
     @Inject(method = "doImpactDamage", at = @At(value = "RETURN"), remap = false)
