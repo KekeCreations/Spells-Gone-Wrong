bc7efa6bb2f10aabe8b1b60ce3fa1471f48d80f3
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2026-07-04 22:57:40 +0100

    V2.0.1

--- src/main/java/com/kekecreations/spells_gone_wrong/core/mixin/SlowSpellMixin.java
+++ src/main/java/com/kekecreations/spells_gone_wrong/core/mixin/SlowSpellMixin.java
@@ -1,18 +1,22 @@
 package com.kekecreations.spells_gone_wrong.core.mixin;
 
 import com.kekecreations.spells_gone_wrong.core.config.SpellsGoneWrongCommonConfig;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.spells.CastSource;
+import io.redspace.ironsspellbooks.api.spells.ICastData;
+import io.redspace.ironsspellbooks.capabilities.magic.TargetEntityCastData;
 import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
 import io.redspace.ironsspellbooks.spells.evocation.SlowSpell;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Mixin(value = SlowSpell.class, remap = false)
 public class SlowSpellMixin {
@@ -27,13 +31,19 @@ public class SlowSpellMixin {
         return 1;
     }
 
-    @Inject(method = "lambda$onCast$0", at = @At(value = "INVOKE", target = "java/util/concurrent/atomic/AtomicInteger.get ()I"))
-    public void spells_Gone_Wrong$onCast(AtomicInteger targets, LivingEntity entity, LivingEntity targetEntity, float radius, int spellLevel, LivingEntity victim, CallbackInfo ci) {
+    @Inject(method = "onCast(Lnet/minecraft/world/level/Level;ILnet/minecraft/world/entity/LivingEntity;Lio/redspace/ironsspellbooks/api/spells/CastSource;Lio/redspace/ironsspellbooks/api/magic/MagicData;)V", at = @At(value = "TAIL"))
+    public void spells_Gone_Wrong$onCast(Level world, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData, CallbackInfo ci) {
         if (SpellsGoneWrongCommonConfig.SLOW_SPELL_CAN_HURT_OWNER.get()) {
-            if (entity.distanceToSqr(targetEntity) < (double)(radius * radius) && entity instanceof Player player && !player.isCreative() && !player.isSpectator()) {
-                entity.addEffect(new MobEffectInstance(MobEffectRegistry.SLOWED, this.getDuration(spellLevel, entity), this.getAmplifier(spellLevel, entity)));
-                targets.incrementAndGet();
+            ICastData data = playerMagicData.getAdditionalCastData();
+            if (data instanceof TargetEntityCastData targetData) {
+                LivingEntity targetEntity = targetData.getTarget((ServerLevel)world);
+                if (targetEntity != null) {
+                    if (entity.distanceToSqr(targetEntity) < 20D && entity instanceof Player player && !player.isCreative() && !player.isSpectator()) {
+                        entity.addEffect(new MobEffectInstance(MobEffectRegistry.SLOWED, this.getDuration(spellLevel, entity), this.getAmplifier(spellLevel, entity)));
+                    }
+                }
             }
+
         }
     }
 }
